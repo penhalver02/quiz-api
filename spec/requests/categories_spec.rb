59416799024e3a4c1bf2f119572e2ec4673da640
Lucas Penhalver <penhalver.lucas@gmail.com>
2024-03-13 18:04:51 -0300

    add test to category serializer

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -2,25 +2,23 @@ require 'rails_helper'
 
 RSpec.describe "Categories", type: :request do
   describe "GET /categories" do
-    context 'succes'  do
-      let!(:category) { Category.create(name: 'matemática') }
-      let!(:second_category) { Category.create(name: 'matemática') }
-      let(:expected_response) do
-        {
-          categories: [
-            { id: category.id, name: category.name },
-            { id: second_category.id, name: second_category.name }
-          ]
-        }.to_json
-      end
+    let!(:category) { Category.create(name: 'matemática') }
+    let!(:second_category) { Category.create(name: 'português') }
+    let(:expected_response) do
+      {
+        categories: [
+          { id: category.id, name: category.name },
+          { id: second_category.id, name: second_category.name }
+        ]
+      }.to_json
+    end
 
-      subject(:request) { get '/categories', headers: { 'ACCEPT' => 'application/json' } }
+    subject(:request) { get '/categories', headers: { 'ACCEPT' => 'application/json' } }
 
-      before { request}
+    before { request}
 
-      it 'returns the categories' do
-        expect(response.body).to eq expected_response
-      end
+    it 'returns the categories' do
+      expect(response.body).to eq expected_response
     end
   end
 end
